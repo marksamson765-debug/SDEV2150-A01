9e5e7bc4b51a1589c26e39199ce102827006b109
oaao <oliver+github@abolish.golf>
2026-03-11 16:34:47 -0600

    add [07 - hooks]: ex02 - useResources - skeleton for API fetching hook'

---
+++ 07 - hooks + lifecycle/examples/02 - component lifecycle/instructor/src/hooks/useResources.js
@@ -0,0 +1,12 @@
+import { useEffect, useState } from 'react';
+
+
+export function useResources() {
+    // 1. I'm going to have state for: resources (holds fetched data), loading state, error msg
+
+    // 2. I'm going to have an async function for fetching data from API (normally in its own layer)
+
+    // 3. We're going to have an effect that fetches resources on page load
+
+    // 4. We'll also have a refetch function
+}
